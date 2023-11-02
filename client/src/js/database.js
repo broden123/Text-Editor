904cb994ad47ed06169ce1e063f87ccaa3b203eb
Brayden Smith <brayden.smithzimmerman@gmail.com>
2023-11-02 15:27:56 -0500

    logic for database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,49 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB("jate", 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// initialize and update database
+export const putDb = async (content) => {
+  // connect to jate db
+  const databaseConnect = await openDB("jate", 1);
+  // create a transaction with database and privledges specified
+  const trans = databaseConnect.transaction("jate", "readwrite");
+  // object store
+  const store = trans.objectStore("jate");
+  // PUT method to store data in database
+  const putInDB = store.put({ id: 1, value: content });
+  // confirm request
+  const confirm = await putInDB;
+  console.log("Stored in Database!", confirm);
+  // error log
+  console.error("putDb not implemented");
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  // connect to jate db
+  const databaseConnect = await openDB("jate", 1);
+  // create a transaction with database and privledges specified
+  const trans = databaseConnect.transaction("jate", "readonly");
+  // object store
+  const store = trans.objectStore("jate");
+  // GET method to store data in database
+  const readDB = store.getAll();
+  // confirm request
+  const confirm = await readDB;
+  console.log("Data Retrieved!", confirm);
+  // error log
+  console.error("getDb not implemented");
+};
 
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,6 +2,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
 const { InjectManifest } = require("workbox-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = () => {
   return {
@@ -21,7 +22,7 @@ module.exports = () => {
       }),
       new MiniCssExtractPlugin(),
       new InjectManifest({
-        swSrc: "./src/src-sw.js",
+        swSrc: "./src-sw.js",
         swDest: "service-worker.js",
       }),
       new WebpackPwaManifest({
